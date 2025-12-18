243e7506e664a9aeb976ac9b00f7e4a78dca293d
Alexby9511 <alexby5911@gmail.com>
2025-12-18 13:57:44 -0500

    Refactor BFS implementation to handle directed graphs and update Arista type

--- index.hs
+++ index.hs
@@ -1,31 +1,38 @@
 type Vertice = Int
-data Arista = Arista Vertice Vertice (Maybe Int) 
+type Arista = (Vertice, Vertice, Int)
 data Grafo = Grafo [Vertice] [Arista] Bool deriving (Show, Eq)
 
 -- Implementación de BFS
 bfs :: Grafo -> Vertice -> [Vertice]
-bfs (Grafo vs as) start = bfsAux [start] []
+bfs (Grafo vs as dir) start = bfsAux [start] []
     where
-        vecinos v = [y | (x, y) <- as, x == v] ++ [x | (x, y) <- as, y == v]
+        vecinos v
+          | dir = [ y | (x, y, _) <- as, x == v ]
+          | otherwise = [ y | (x, y, _) <- as, x == v ] ++ [ x | (x, y, _) <- as, y == v ]
+
         bfsAux [] visitados = visitados
         bfsAux (q:qs) visitados
             | q `elem` visitados = bfsAux qs visitados
             | otherwise = bfsAux (qs ++ nuevos) (visitados ++ [q])
-            where nuevos = filter (`notElem` visitados) (vecinos q)
+            where nuevos = filter (`notElem` (visitados ++ [q])) (vecinos q)
 
 
--- Verifica si un grafo es conexo usando BFS
+-- Verifica si un grafo no dirigido es conexo usando BFS
 isConexo :: Grafo -> Bool
-isConexo (Grafo [] _) = True
-isConexo g@(Grafo (v:vs) _) =
+isConexo (Grafo [] _ _) = True
+isConexo g@(Grafo (v:vs) _ _) =
     let visitados = bfs g v
     in all (`elem` visitados) (v:vs)
+
+-- Implementación de DFS
+-- dfs :: Grafo -> Vertice -> [Vertice]
+-- dfs (Grafo vs as dir) start = dfsAux [start] []
 
 main :: IO ()
 main = do
     let vertices = [1,2,3,4,5,6]
-        aristas = [(1,2), (2,4), (2,5), (5,4), (1,3), (3,6)]
-        grafo = Grafo vertices aristas
+        aristas = [(1,2,1), (2,4,1), (2,5,1), (5,4,1), (1,3,1), (3,6,1)]
+        grafo = Grafo vertices aristas True
         resultadoBFS = bfs grafo 5
         conexo = isConexo grafo
     putStrLn $ "Recorrido BFS desde 5: " ++ show resultadoBFS
