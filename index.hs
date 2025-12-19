66e8956e00633a370a0f35a47db7a7465b7d677f
Alexby9511 <alexby5911@gmail.com>
2025-12-19 04:23:18 -0500

    Refactor BFS and DFS implementations to use the Grafo parameter directly; add functions to calculate minimum and maximum degree, check if the graph is regular, and determine the independence number.

--- index.hs
+++ index.hs
@@ -10,13 +10,13 @@ vecinos (Grafo _ as dir) v
 
 -- Implementación de BFS
 bfs :: Grafo -> Vertice -> [Vertice]
-bfs (Grafo vs as dir) start = bfsAux [start] []
+bfs g@(Grafo vs as dir) start = bfsAux [start] []
     where
         bfsAux [] visitados = visitados
         bfsAux (q:qs) visitados
             | q `elem` visitados = bfsAux qs visitados
             | otherwise = bfsAux (qs ++ nuevos) (visitados ++ [q])
-            where nuevos = filter (`notElem` (visitados ++ [q])) (vecinos (Grafo vs as dir) q)
+            where nuevos = filter (`notElem` (visitados ++ [q])) (vecinos g q)
 
 
 -- Verifica si un grafo no dirigido es conexo usando BFS
@@ -28,21 +28,49 @@ isConexo g@(Grafo (v:vs) _ _) =
 
 -- Implementación de DFS
 dfs :: Grafo -> Vertice -> [Vertice]
-dfs (Grafo vs as dir) start = dfsAux [start] []
+dfs g@(Grafo vs as dir) start = dfsAux [start] []
     where
         dfsAux [] visitados = visitados
         dfsAux (s:ss) visitados
             | s `elem` visitados = dfsAux ss visitados
-            | otherwise = dfsAux (vecinos (Grafo vs as dir) s ++ ss) (visitados ++ [s])
+            | otherwise = dfsAux (vecinos g s ++ ss) (visitados ++ [s])
+
+-- Halla el grado mínimo y máximo de un grafo no dirigido
+gradoMinMax :: Grafo -> (Int, Int)
+gradoMinMax g@(Grafo vs as dir) = (minimum grados, maximum grados)
+    where
+        grados = [length (vecinos g v) | v <- vs]
+
+-- Verifica si un grafo no dirigido es regular
+isRegular :: Grafo -> Bool
+isRegular grafo = 
+    let (minG, maxG) = gradoMinMax grafo
+    in minG == maxG
+
+-- Halla el número de independencia de un grafo no dirigido (número máximo de vértices sin aristas entre ellos)
+numeroIndependencia :: Grafo -> Int
+numeroIndependencia g@(Grafo vs as dir) = numeroIndependenciaAux g vs
+    where
+        numeroIndependenciaAux _ [] = 0
+        numeroIndependenciaAux grafo (v:vs) = 
+            let sinVecinos = filter (`notElem` vecinos grafo v) vs
+            in 1 + numeroIndependenciaAux grafo sinVecinos
+
+
+-- Implementacion de árbol de expansión mínima 
 
 main :: IO ()
 main = do
-    let vertices = [1,2,3,4,5,6]
-        aristas = [(1,2,1), (2,4,1), (2,5,1), (5,4,1), (1,3,1), (3,6,1)]
+    let vertices = [1,2,3,4,5,6,7]
+        aristas = [(1,2,1), (2,3,1), (3,4,1), (5,4,1), (7,6,1)]
         grafo = Grafo vertices aristas False
         resultadoBFS = bfs grafo 5
         resultadoDFS = dfs grafo 5
         conexo = isConexo grafo
+        regular = isRegular grafo
+        numInd = numeroIndependencia grafo
     putStrLn $ "Recorrido BFS desde 5: " ++ show resultadoBFS
     putStrLn $ "Recorrido DFS desde 5: " ++ show resultadoDFS
     putStrLn $ "¿El grafo es conexo? " ++ show conexo
+    putStrLn $ "¿El grafo es regular? " ++ show regular
+    putStrLn $ "Número de independencia del grafo: " ++ show numInd
